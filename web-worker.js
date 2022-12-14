8a460c9eeb1c0c3bdbc8d2530894a9fd36260163
Caique de Castro <castro.caique@gmail.com>
2022-12-14 07:38:16 -0300

    feat: Return timing from web worker

--- web-worker.js
+++ web-worker.js
@@ -2,18 +2,20 @@ const form = document.forms.fibonacci;
 const resultContainer = document.querySelector('#result');
 const timingContainer = document.querySelector('#timing');
 
-const worker = new Worker("worker.js");
+const worker = new Worker('worker.js');
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
   const input = formData.get('input');
 
   resultContainer.textContent = '';
+  timingContainer.textContent = '';
   worker.postMessage(+input);
 
-  worker.onmessage = (e) => {
-    console.log("Received message from web worker");
-    resultContainer.textContent = e.data;
-  }
+  worker.addEventListener('message', (e) => {
+    console.log('Received message from web worker');
+    resultContainer.textContent = e.data.result;
+    timingContainer.textContent = e.data.time;
+  });
 });
--- worker.js
+++ worker.js
@@ -1,7 +1,14 @@
 importScripts('functions.js');
 
-self.onmessage = (e) => {
-  console.log("Web worker received message", e);
+self.addEventListener('message', (e) => {
+  console.log('Web worker received message', e);
 
-  postMessage(calculateFibonacciRecursive(e.data));
-}
+  const startTime = performance.now();
+  const result = calculateFibonacciRecursive(e.data);
+  const endTime = performance.now();
+
+  self.postMessage({
+    result,
+    time: endTime - startTime,
+  });
+});
