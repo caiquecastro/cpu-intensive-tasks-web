699bd32a8a5a42af0174bafcf686587cd24ded21
Caique de Castro <castro.caique@gmail.com>
2022-12-28 20:13:35 -0300

    feat: Use unsigned integer for fibonacci sequence

--- fibonacci.c
+++ fibonacci.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int fibonacci(int number) {
+unsigned long fibonacci(unsigned int number) {
     if (number < 2) {
         return number;
     }
     return fibonacci(number - 1) + fibonacci(number - 2);
 }
 
-int main() {
-    printf("%d\n", fibonacci(40));
+int main(int argc, char *argv[]) {
+    unsigned long n = strtoul(argv[1], NULL, 10);
+    for (unsigned int i = 1; i <= n; i++) {
+        printf("Fibonacci sequence number at index %u is %lu\n", i, fibonacci(i));
+    }
     return 0;
 }
