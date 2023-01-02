028aca4a3e954f2969830efa3a5c979d306d8ea7
Caique de Castro <castro.caique@gmail.com>
2023-01-02 09:45:19 -0300

    feat: Add web assembly version page

---
+++ public/web-assembly.js
@@ -0,0 +1,27 @@
+const form = document.forms.fibonacci;
+
+let wasmObject;
+
+WebAssembly.instantiateStreaming(fetch("program.wasm"))
+  .then((obj) => {
+    wasmObject = obj;
+  });
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const input = formData.get('input');
+  const memoize = !!formData.get('memoize');
+
+  cleanResult();
+  showSpinner();
+  const startTime = performance.now();
+  const result = wasmObject.instance.exports.fibonacci(input);
+  showResult({
+    result,
+    time: performance.now() - startTime,
+  });
+});
+
+
+
