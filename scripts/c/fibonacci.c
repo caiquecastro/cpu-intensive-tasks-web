c1c5090b3b13ed24fe029a5dcdd41f73f176d52f
Caique de Castro <castro.caique@gmail.com>
2023-01-06 08:25:49 -0300

    build: Move c script to folder

--- scripts/c/fibonacci.c
+++ scripts/c/fibonacci.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,7 +10,18 @@ unsigned long fibonacci(unsigned int number) {
 }
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Provide an argument for fibonacci sequence\n");
+        return 1;
+    }
+
     unsigned long n = strtoul(argv[1], NULL, 10);
+
+    if (errno != 0) {
+        printf("Invalid argument for fibonacci sequence\n");
+        return 1;
+    }
+
     for (unsigned int i = 1; i <= n; i++) {
         printf("Fibonacci sequence number at index %u is %lu\n", i, fibonacci(i));
     }
