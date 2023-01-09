981b21c958bf6653cbfcdf2da2d1e64e9ee81cd3
Caique de Castro <castro.caique@gmail.com>
2023-01-09 11:10:01 -0300

    fix: Validate argv on python script

--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -7,6 +7,10 @@ def fibonacci(number: int):
     return fibonacci(number - 1) + fibonacci(number - 2)
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Provide an argument for fibonacci sequence", file=sys.stderr)
+        sys.exit(1)
+
     n = int(sys.argv[1])
     for i in range(1, n + 1):
         print(f"Fibonacci sequence number at index {i} is {fibonacci(i)}")
